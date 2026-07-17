e92fa1b66f7470303bcf2e62523d6b2e55701b0a
Eduardo Venegas <Eduardo.Venegas-Hernandez@bmw.de>
2026-07-17 13:07:08 +0200

    Update experience

--- src/components/organisms/Skills.organism.jsx
+++ src/components/organisms/Skills.organism.jsx
@@ -5,9 +5,9 @@ import SkillsData from "../../assets/data/skills.json";
 function Skills() {
   return (
     <div className="flex flex-col gap-6 px-4">
-      <SkillSection title="Lib / Frameworks" skills={SkillsData.frameworks} />
-      <SkillSection title="Tools" skills={SkillsData.tools} />
-      <SkillSection title="Languages" skills={SkillsData.languages} />
+      <SkillSection title="AI & Data" skills={SkillsData.aiData} />
+      <SkillSection title="Software & Frameworks" skills={SkillsData.softwareFrameworks} />
+      <SkillSection title="Cloud & MLOps" skills={SkillsData.cloudMLOps} />
     </div>
   );
 }
